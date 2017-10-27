199f89e7456e943f478c44a85100ca7528e5d77a
Easwari Ramani <eshuprad@gmail.com>
2017-10-27 19:31:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function updateObjectWithKeyAndValue(object, key, value){
   return obj;
 }
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
-
+  object[key]=value;
   return object;
 }
 function deleteFromObjectByKey(object, key){

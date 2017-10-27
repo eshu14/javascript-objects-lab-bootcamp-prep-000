3a41d7d4f66a415249f3f66e7991b722651695f5
Easwari Ramani <eshuprad@gmail.com>
2017-10-27 18:51:38 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,27 @@
+var recipes={
+  nam : "Eshu",
+  age: 42,
+  sex: "female"
+};
+function updateObjectWithKeyAndValue(object, key, value){
+  object[key]=value;
+  return object;
+}
+function updateObjectWithKeyAndValue(object, key, value){
+  var obj=Object.assign({},object);
+  return obj;
+
+  };
+
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+
+  return object;
+}
+function deleteFromObjectByKey(object, key){
+
+  return obj1;
+}
+function destructivelyDeleteFromObjectByKey(object, key){
+
+}

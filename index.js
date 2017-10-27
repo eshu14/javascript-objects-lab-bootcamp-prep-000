c26f97a99db67e36c0d62d0aea60ac85c8f60825
Easwari Ramani <eshuprad@gmail.com>
2017-10-27 18:55:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ function updateObjectWithKeyAndValue(object, key, value){
   return object;
 }
 function updateObjectWithKeyAndValue(object, key, value){
+  object[key]=value;
   var obj=Object.assign({},[key:value]);
   return obj;
 
